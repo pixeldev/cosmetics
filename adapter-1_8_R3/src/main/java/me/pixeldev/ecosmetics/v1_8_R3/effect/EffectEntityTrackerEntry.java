78cdb1b1848655e44d19c85fe279d8f9d218fabc
pixeldev <amiranda8695@gmail.com>
2021-11-09 11:17:26 -0600

    fix: now effects apply cosmetics visibility

--- adapter-1_8_R3/src/main/java/me/pixeldev/ecosmetics/v1_8_R3/effect/EffectEntityTrackerEntry.java
+++ adapter-1_8_R3/src/main/java/me/pixeldev/ecosmetics/v1_8_R3/effect/EffectEntityTrackerEntry.java
@@ -1,10 +1,12 @@
 package me.pixeldev.ecosmetics.v1_8_R3.effect;
 
 import me.pixeldev.ecosmetics.api.cosmetic.effect.EffectCosmetic;
+import me.pixeldev.ecosmetics.api.cosmetic.event.PreAddCosmeticSpectatorEvent;
 import me.pixeldev.ecosmetics.v1_8_R3.track.AbstractEntityTrackerEntry;
 
 import net.minecraft.server.v1_8_R3.EntityPlayer;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 
 public class EffectEntityTrackerEntry
@@ -23,6 +25,16 @@ public class EffectEntityTrackerEntry
 
 	@Override
 	protected void show(EntityPlayer player) {
+		PreAddCosmeticSpectatorEvent event = new PreAddCosmeticSpectatorEvent(
+			player.getBukkitEntity(), effectCosmetic
+		);
+
+		Bukkit.getPluginManager().callEvent(event);
+
+		if (event.isCancelled()) {
+			return;
+		}
+
 		effectCosmetic.getSpectators().addSpectator(
 			player.getBukkitEntity()
 		);
